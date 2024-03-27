b98a6a8c9c980c7d6ebad181b089e40f2201ef14
Jingjing Li <jl3676@cornell.edu>
2024-03-26 23:10:21 -0700

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -27,7 +27,7 @@ def option_model_nllh(params, D, structure, meta_learning=True):
 	TS_2s[0,:,:] = np.ones((2,4)) / 4
 	nC_2 = 2 * num_block
 	PTS_2 = np.ones((nTS_2,nC_2))
-	p_policies = np.array([1-eps-prior, prior, eps]) if meta_learning else np.array([0.0, 0.0, 1.0])
+	p_policies = np.array([1-eps_meta-prior, prior, eps_meta]) if meta_learning else np.array([0.0, 0.0, 1.0])
 	encounter_matrix_2 = np.zeros(nC_2)
 
 	for t in range(D.shape[0]):	
@@ -223,7 +223,7 @@ def option_model(num_subject, alpha_1, alpha_2, alpha_cf, beta_1, beta_2, concen
 		PTS_2 = np.ones((nTS_2,nC_2))
 		
 		# compression over stage 1, compression over stage 2, full hierarchical
-		p_policies = np.array([1-eps-prior, prior, eps]) if meta_learning else np.array([0.0, 0.0, 1.0])
+		p_policies = np.array([1-eps_meta-prior, prior, eps_meta]) if meta_learning else np.array([0.0, 0.0, 1.0])
 		encounter_matrix = np.zeros(nC)
 		encounter_matrix_2 = np.zeros(nC_2)
 
