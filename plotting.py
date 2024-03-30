6760ea97cc9989f9f5e42971d0e11e7c23103a7f
Jingjing Li <jl3676@cornell.edu>
2024-03-30 13:47:23 -0700

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -178,7 +178,8 @@ def plot_validation_PTS(data_sim, m, ntrials=1, save_vector=False):
     vmax = 1
     c = 180
     # cmap = sns.diverging_palette(c, c+180, s=100, as_cmap=True)
-    cmap = sns.color_palette("Greys", as_cmap=True)
+    # cmap = sns.color_palette("Greys", as_cmap=True)
+    cmap = sns.light_palette("seagreen", as_cmap=True)
     sns.heatmap(average_probabilities,vmin=vmin,vmax=vmax,cmap=cmap,square=True)
     plt.xticks(np.arange(12)*2+1, np.arange(1,13), rotation=0)
     plt.yticks(np.arange(12)*2+0.5, np.arange(12)*2+1)
