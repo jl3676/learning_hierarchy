37e8e352befaf57cf19688d5aa273518259acbf8
Jingjing Li <jl3676@cornell.edu>
2024-03-31 14:32:32 -0700

    qc

--- modeling.py
+++ modeling.py
@@ -130,10 +130,6 @@ def option_model_nllh(params, D, structure, meta_learning=True):
 			PTS_2[:,c_2] = reg / np.sum(reg)
 				
 			TS_2s[:,state,a_2-1] += alpha_2 * (r_2 - TS_2s[:,state,a_2-1]) * PTS_2[:,c_2]
-			# if r_2:
-			# 	TS_2s[:,1-state,a_2-1] -= alpha_cf * alpha_2 * TS_2s[:,1-state,a_2-1] * PTS_2[:,c_2]
-			# 	TS_2s[:,0,a_2-1] -= alpha_cf * alpha_2 * TS_2s[:,0,a_2-1] * PTS_2[:,c_2_alt]
-			# 	TS_2s[:,1,a_2-1] -= alpha_cf * alpha_2 * TS_2s[:,1,a_2-1] * PTS_2[:,c_2_alt]
 
 			p_policies[0] *= pchoice_2_compress_1[a_2-1]
 			p_policies[1] *= pchoice_2_compress_2[a_2-1]
@@ -146,7 +142,7 @@ def option_model_nllh(params, D, structure, meta_learning=True):
 	return -llh
 
 
-def option_model(num_subject, alpha_1, alpha_2, beta_1, beta_2, concentration_1, concentration_2, epsilon, prior, experiment, structure, meta_learning=True, debug=False):
+def option_model(num_subject, alpha_1, alpha_2, beta_1, beta_2, concentration_1, concentration_2, epsilon, prior, experiment, structure, meta_learning=True):
 	'''
 	Fits the option model to the data of the OT-CA1-CA1 task.
 
@@ -167,7 +163,7 @@ def option_model(num_subject, alpha_1, alpha_2, beta_1, beta_2, concentration_1,
 		- num_trial_finished: the sample sizes of the above means
 	'''
 	num_block = 12
-	num_trial_12 = 32
+	num_trial_12 = 60
 	num_trial_else = 32
 
 	# eps_meta = 10**eps_meta if meta_learning else 0.0
@@ -423,7 +419,6 @@ def option_model(num_subject, alpha_1, alpha_2, beta_1, beta_2, concentration_1,
 
 						# Use the result observed to infer the current TS again
 						TS_2 = np.argmax(PTS_2[:,c_2])
-						TS_2_alt = np.argmax(PTS_2[:,c_2_alt])
 						TS_2s[TS_2,state,a_2-1] += alpha_2 * (correct_2 - TS_2s[TS_2,state,a_2-1])
 
 						p_policies[0] *= pchoice_2_compress_1[a_2-1]
