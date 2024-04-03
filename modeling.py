c2197a76203478ee4ca76beb8bf4f558d5fc805e
Jingjing Li <jl3676@cornell.edu>
2024-04-03 15:05:20 -0700

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -70,7 +70,8 @@ def option_model_nllh(params, D, structure, meta_learning=True):
 			PTS_2[:,c_2] += 1e-6
 			PTS_2[:,c_2] /= np.sum(PTS_2[:,c_2])
 
-			TS_2s[:,state,a_2-1] += alpha_2 * (r_2 - TS_2s[:,state,a_2-1]) * PTS_2[:,c_2]
+			# TS_2s[:,state,a_2-1] += alpha_2 * (r_2 - TS_2s[:,state,a_2-1]) * PTS_2[:,c_2]
+			TS_2s[0,state,a_2-1] += alpha_2 * (r_2 - TS_2s[0,state,a_2-1])
 
 	return -llh
 
