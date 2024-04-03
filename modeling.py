7b79ce90c9046ebede862a1a5528b12e1ad50659
Jingjing Li <jl3676@cornell.edu>
2024-04-03 14:41:09 -0700

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -70,6 +70,8 @@ def option_model_nllh(params, D, structure, meta_learning=True):
 
 			TS_2s[:,state,a_2-1] += alpha_2 * (r_2 - TS_2s[:,state,a_2-1]) * PTS_2[:,c_2]
 
+	print(PTS_2[:,:2])
+
 	return -llh
 
 
@@ -271,8 +273,6 @@ def option_model(num_subject, params, experiment, structure, meta_learning=True)
 	data = {'tr': tr, 'a':a, 'r':r, 's1':s1, 's2':s2, 'counter1':population_counter1, 'counter2':population_counter2, \
 		 'counter12_12':counter12_12, 'a_12_12':a_12_12, 's_12_12':s_12_12, 'r_12_12':r_12_12, \
 			'p_policies_history': p_policies_history, 'TS_2_history': TS_2_history} 	
-	
-	print(PTS_2[:,:2])
 	return data
 
 
