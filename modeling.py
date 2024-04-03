2d8be2700e010cb83c4728f661b4746b0f44a110
Jingjing Li <jl3676@cornell.edu>
2024-04-03 11:39:04 -0700

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -26,6 +26,7 @@ def option_model_nllh(params, D, structure, meta_learning=True):
 	nC_2 = 2 * num_block
 	PTS_2 = np.ones((nTS_2,nC_2))
 	encounter_matrix_2 = np.zeros(nC_2)
+	encounter_matrix_2[0] = 1
 
 	for t in range(D.shape[0]):	
 		stage = int(D[t,1])
@@ -60,13 +61,10 @@ def option_model_nllh(params, D, structure, meta_learning=True):
 			c_2_alt = block * 2 + (1 - cue)
 			for this_c_2 in sorted([c_2, c_2_alt]):
 				if encounter_matrix_2[this_c_2] == 0:
-					if this_c_2 > 0:
-						PTS_2 = new_SS_update_option(PTS_2, this_c_2, concentration_2)
-						TS_2s = np.vstack((TS_2s, [np.ones((2,4)) / 4])) # initialize Q-values for new TS creation
-						nTS_2 += 1
+					PTS_2 = new_SS_update_option(PTS_2, this_c_2, concentration_2)
+					TS_2s = np.vstack((TS_2s, [np.ones((2,4)) / 4])) # initialize Q-values for new TS creation
+					nTS_2 += 1
 					encounter_matrix_2[this_c_2] = 1
-
-			lt_2 = 0
 
 			Q_full = TS_2s[:, state]
 			pchoice_2_full = softmax(beta_2 * Q_full, axis=-1)
@@ -144,6 +142,7 @@ def option_model(num_subject, params, experiment, structure, meta_learning=True)
 		
 		# compression over stage 1, compression over stage 2, full hierarchical
 		encounter_matrix_2 = np.zeros(nC_2)
+		encounter_matrix_2[0] = 1
 
 		# 3. start looping over all blocks
 		for block in range(num_block):
@@ -194,10 +193,9 @@ def option_model(num_subject, params, experiment, structure, meta_learning=True)
 				while correct_2 == 0 and counter_2 < 10:
 					for this_c_2 in sorted([c_2, c_2_alt]):
 						if encounter_matrix_2[this_c_2] == 0:
-							if this_c_2 > 0:
-								PTS_2 = new_SS_update_option(PTS_2, this_c_2, concentration_2)
-								TS_2s = np.vstack((TS_2s, [np.ones((2,4)) / 4])) # initialize Q-values for new TS creation
-								nTS_2 += 1
+							PTS_2 = new_SS_update_option(PTS_2, this_c_2, concentration_2)
+							TS_2s = np.vstack((TS_2s, [np.ones((2,4)) / 4])) # initialize Q-values for new TS creation
+							nTS_2 += 1
 							encounter_matrix_2[this_c_2] = 1
 
 					Q_full = TS_2s[:, state]
