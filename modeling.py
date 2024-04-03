4eee2fb2843fe8a84b5bafa16c54c1cecf840144
Jingjing Li <jl3676@cornell.edu>
2024-04-02 20:56:55 -0700

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -10,6 +10,7 @@ def option_model_nllh(params, D, structure, meta_learning=True):
 	'''
 	[alpha_2, beta_2, concentration_2, epsilon] = params
 	# alpha_2 = 1
+	beta_2 = 5
 	prior = 0.25
 	# eps_meta = 10**eps_meta if meta_learning else 0.0
 	concentration_2 = 10**concentration_2
@@ -162,7 +163,7 @@ def option_model_nllh(params, D, structure, meta_learning=True):
 	return -llh
 
 
-def option_model(num_subject, alpha_2, beta_2, concentration_2, epsilon, experiment, structure, meta_learning=True):
+def option_model(num_subject, alpha_2, concentration_2, epsilon, experiment, structure, meta_learning=True):
 	'''
 	Fits the option model to the data of the OT-CA1-CA1 task.
 
@@ -186,7 +187,7 @@ def option_model(num_subject, alpha_2, beta_2, concentration_2, epsilon, experim
 	num_trial_12 = 60
 	num_trial_else = 32
 	alpha_1 = 1
-	beta_1 = 10
+	beta_1 = beta_2 = 5
 	concentration_1 = 0.2
 	prior = 0.25
 
@@ -745,6 +746,6 @@ def parallel_worker(args):
 
 
 def parallel_simulator(args):
-    this_model, i, niters_sim, alpha_2, beta_2, concentration_2, epsilon, exp, structure, meta_learning = args
-    this_data = globals()[this_model](niters_sim, alpha_2, beta_2, concentration_2, epsilon, exp, structure, meta_learning)
+    this_model, i, niters_sim, alpha_2, concentration_2, epsilon, exp, structure, meta_learning = args
+    this_data = globals()[this_model](niters_sim, alpha_2, concentration_2, epsilon, exp, structure, meta_learning)
     return i, this_data
