8a988bf63860251dde5eb82516c28b9d1e7711ec
Jingjing Li <jl3676@cornell.edu>
2024-04-03 15:25:51 -0700

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -56,9 +56,9 @@ def option_model_nllh(params, D, structure, meta_learning=True):
 			llh += np.log(pchoice_2[a_2-1])
 
 			if r_2 == 0:
-				PTS_2[:,c_2] *= (1 - TS_2s[:,state,a_2-1])
+				PTS_2[:,c_2] *= (1 - pchoice_2_full[:,a_2-1])
 			else:
-				PTS_2[:,c_2] *= TS_2s[:,state,a_2-1] 
+				PTS_2[:,c_2] *= pchoice_2_full[:,a_2-1]	
 			PTS_2[:,c_2] += 1e-6
 			PTS_2[:,c_2] /= np.sum(PTS_2[:,c_2])
 
@@ -183,9 +183,9 @@ def option_model(num_subject, params, experiment, structure, meta_learning=True)
 
 					# Use the result to update PTS_2 with Bayes Rule
 					if correct_2 == 0:
-						PTS_2[:,c_2] *= (1 - TS_2s[:,state,a_2-1])
+						PTS_2[:,c_2] *= (1 - pchoice_2_full[:,a_2-1])
 					else:
-						PTS_2[:,c_2] *= TS_2s[:,state,a_2-1] 
+						PTS_2[:,c_2] *= pchoice_2_full[:,a_2-1]
 					PTS_2[:,c_2] += 1e-6
 					PTS_2[:,c_2] /= np.sum(PTS_2[:,c_2])
 
