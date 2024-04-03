3c29e3b38356a044453391dff7b26715d7e8df89
Jingjing Li <jl3676@cornell.edu>
2024-04-03 14:39:49 -0700

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -271,6 +271,8 @@ def option_model(num_subject, params, experiment, structure, meta_learning=True)
 	data = {'tr': tr, 'a':a, 'r':r, 's1':s1, 's2':s2, 'counter1':population_counter1, 'counter2':population_counter2, \
 		 'counter12_12':counter12_12, 'a_12_12':a_12_12, 's_12_12':s_12_12, 'r_12_12':r_12_12, \
 			'p_policies_history': p_policies_history, 'TS_2_history': TS_2_history} 	
+	
+	print(PTS[:,:2])
 	return data
 
 
