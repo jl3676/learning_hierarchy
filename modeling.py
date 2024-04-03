cc70afbaefb4aefb8a67817921e1d699af829ce2
Jingjing Li <jl3676@cornell.edu>
2024-04-03 15:47:10 -0700

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -186,7 +186,7 @@ def option_model(num_subject, params, experiment, structure, meta_learning=True)
 					PTS_2[:,c_2] += 1e-6
 					PTS_2[:,c_2] /= np.sum(PTS_2[:,c_2])
 
-					TS_2s[c_2,state,a_2-1] += alpha_2 * (correct_2 - TS_2s[c_2,state,a_2-1]) # * PTS_2[:,c_2]
+					TS_2s[cue,state,a_2-1] += alpha_2 * (correct_2 - TS_2s[cue,state,a_2-1]) # * PTS_2[:,c_2]
 
 				# Record variables per trial
 				counter_1_temp[trial] = counter_1
