4eb5d58ab0768044fd7f81dd1655d96cc7bdbb34
Jingjing Li <jl3676@cornell.edu>
2024-04-03 12:37:55 -0700

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -17,13 +17,13 @@ def option_model_nllh(params, D, structure, meta_learning=True):
 	s_2 = a_2 = -1
 	block = -1
 
-	nTS_2 = 1 # initialize the number of task-set in the second stage
+	nTS_2 = 2 # initialize the number of task-set in the second stage
 	TS_2s = np.ones((nTS_2,2,4)) / 4
 	nC_2 = 2 * num_block
 	PTS_2 = np.zeros((nTS_2,nC_2)) 
-	PTS_2[0] = 1
-	# PTS_2[0,0::2] = 1
-	# PTS_2[1,1::2] = 1
+	# PTS_2[0] = 1
+	PTS_2[0,0::2] = 1
+	PTS_2[1,1::2] = 1
 	encounter_matrix_2 = np.zeros(nC_2)
 	encounter_matrix_2[:nTS_2] = 1
 
@@ -125,12 +125,12 @@ def option_model(num_subject, params, experiment, structure, meta_learning=True)
 		a_all = np.empty((num_block-2,num_trial_else),dtype='object')
 		r_all = np.empty_like(a_all,dtype='object')
 
-		nTS_2 = 1 # initialize the number of task-set in the second stage
+		nTS_2 = 2 # initialize the number of task-set in the second stage
 		TS_2s = np.ones((nTS_2,2,4)) / 4
 		PTS_2 = np.zeros((nTS_2,nC_2)) 
-		PTS_2[0] = 1
-		# PTS_2[0,0::2] = 1
-		# PTS_2[1,1::2] = 1
+		# PTS_2[0] = 1
+		PTS_2[0,0::2] = 1
+		PTS_2[1,1::2] = 1
 		encounter_matrix_2 = np.zeros(nC_2)
 		encounter_matrix_2[:nTS_2] = 1
 
