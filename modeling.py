8141d2ea5c9eb5f4a32c5349be6b53f10ebbc9a7
Jingjing Li <jl3676@cornell.edu>
2024-04-03 15:21:42 -0700

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -21,7 +21,6 @@ def option_model_nllh(params, D, structure, meta_learning=True):
 	nC_2 = 2 * num_block
 	PTS_2 = np.zeros((nTS_2,nC_2)) 
 	PTS_2[0] = 1
-	print(PTS_2)
 	# PTS_2[0,0::2] = 1
 	# PTS_2[1,1::2] = 1
 	encounter_matrix_2 = np.zeros(nC_2)
@@ -123,7 +122,7 @@ def option_model(num_subject, params, experiment, structure, meta_learning=True)
 		nTS_2 = 2 # initialize the number of task-set in the second stage
 		TS_2s = np.ones((nTS_2,2,4)) / 4
 		PTS_2 = np.zeros((nTS_2,nC_2)) 
-		PTS_2[0] = 1
+		# PTS_2[0] = 1
 		PTS_2[0,0::2] = 1
 		PTS_2[1,1::2] = 1
 		encounter_matrix_2 = np.zeros(nC_2)
