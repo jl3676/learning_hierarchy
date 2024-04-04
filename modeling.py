02b146db1a55df9be64b6d3edd944ac68fd13347
Jingjing Li <jl3676@cornell.edu>
2024-04-03 18:16:10 -0700

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -61,11 +61,11 @@ def option_model_nllh(params, D, structure, meta_learning=True):
 
 			Q_full = TS_2s[:, state]
 			if len(actions_tried) > 0:
-				Q_full[:,list(actions_tried)] = 0
+				Q_full[:,list(actions_tried)] = epsilon
 			pchoice_2_full = softmax(beta_2 * Q_full, axis=-1)
 			pchoice_2 = np.sum(pchoice_2_full * PTS_2[:,c_2].reshape(-1,1), axis=0)
 
-			llh += np.log(pchoice_2[a_2-1] * (1 - epsilon) + epsilon / 4)
+			llh += np.log(pchoice_2[a_2-1])
 
 			if r_2 == 0:
 				PTS_2[:,c_2] *= (1 - TS_2s[:,state,a_2-1])
@@ -199,9 +199,9 @@ def option_model(num_subject, params, experiment, structure, meta_learning=True)
 					TS_2 = np.random.choice(np.arange(PTS_2.shape[0]), 1, p=PTS_2[:,c_2])[0]
 					Q_full = TS_2s[TS_2, state]
 					if len(actions_tried) > 0:
-						Q_full[list(actions_tried)] = 0
+						Q_full[list(actions_tried)] = epsilon
 					pchoice_2_full = softmax(beta_2 * Q_full)
-					pchoice_2 = pchoice_2_full * (1-epsilon) + epsilon / 4
+					pchoice_2 = pchoice_2_full 
 
 					a_2 = np.random.choice(np.arange(1,5), 1, p=pchoice_2)[0]
 					actions_tried.add(a_2-1)
