3fde763b21ab31ab3b776da752532bdb03525d49
Jingjing Li <jl3676@cornell.edu>
2024-04-07 14:32:20 -0700

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -198,7 +198,7 @@ def option_model(num_subject, params, experiment, structure, meta_learning=True)
 		encounter_matrix_2 = np.zeros(nC_2)
 		encounter_matrix_2[:nTS_2] = 1
 		if meta_learning:
-			eps_meta = 0.01
+			eps_meta = 0.1
 			# prior = 0.1
 			beta_policies = 50 # hard max
 			p_policies = np.array([1-eps_meta-prior, prior, eps_meta])
