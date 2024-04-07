f81b25390c7d10bf504871829f54b1a1538674bf
Jingjing Li <jl3676@cornell.edu>
2024-04-07 14:08:29 -0700

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -326,7 +326,7 @@ def option_model(num_subject, params, experiment, structure, meta_learning=True)
 
 					if meta_learning:
 						if len(actions_tried) == 1:
-							p_policies_history[sub,block,trial] = p_policies_softmax
+							p_policies_history[sub,block,trial] = p_policies
 						p_policies[0] *= pchoice_2_compress_1[a_2-1]
 						p_policies[1] *= pchoice_2_compress_2[a_2-1]
 						p_policies[2] *= pchoice_2_full[a_2-1]
@@ -335,6 +335,7 @@ def option_model(num_subject, params, experiment, structure, meta_learning=True)
 							p_policies += eps_meta
 						p_policies /= np.sum(p_policies)
 						p_policies_softmax = softmax(beta_policies * p_policies)
+						print(f'beta: {beta_policies}, p_policies: {p_policies}, p_policies_softmax: {p_policies_softmax')
 					# if len(actions_tried) == 1:
 					# 	p_policies_history[sub,block,trial,0] = pchoice_2[a_2-1] if not meta_learning else p_policies_softmax[-1]
 					# 	p_policies_history[sub,block,trial,1] = pchoice_2[correct_action_2-5]
