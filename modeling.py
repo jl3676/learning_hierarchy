4070723459e6f849300f7276f1ac197c39dd6a24
Jingjing Li <jl3676@cornell.edu>
2024-04-07 15:36:49 -0700

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -335,12 +335,12 @@ def option_model(num_subject, params, experiment, structure, meta_learning=True)
 							p_policies += eps_meta
 						p_policies /= np.sum(p_policies)
 						p_policies_softmax = softmax(beta_policies * p_policies)
-						if block > 10 and len(actions_tried) < 2:
+						if block > 10 and trial > 30 and len(actions_tried) < 2:
 							if p_policies_softmax[-1] > 0.9:
 								p_policies_history = np.full((num_subject,num_block,num_trial_12,3), np.nan)
 								TS_2_history = np.full((num_subject,num_block*2,num_trial_12), np.nan)
 							else:
-								print(f'p_policies: {p_policies}, compress_1: {pchoice_2_compress_1[a_2-1]}, compress_2: {pchoice_2_compress_2[a_2-1]}, full: {pchoice_2_full[a_2-1]}')
+								print(f'p_policies: {p_policies}, compress_1: {pchoice_2_compress_1[a_2-1]}, compress_2: {pchoice_2_compress_2[a_2-1]}, full: {pchoice_2_full[a_2-1]}, TS_2s: {TS_2s[:2]}')
 					# if len(actions_tried) == 1:
 					# 	p_policies_history[sub,block,trial,0] = pchoice_2[a_2-1]
 					# 	p_policies_history[sub,block,trial,1] = pchoice_2[correct_action_2-5]
