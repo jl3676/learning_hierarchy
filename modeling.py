06b56ce9be32433a16b37a23db094eded5c0f578
Jingjing Li <jl3676@cornell.edu>
2024-08-16 09:53:17 -0500

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -17,7 +17,7 @@ def abstraction_model_nllh(params, D, structure, meta_learning=True):
 	'''
 	# unpack the parameters
 	[alpha_2, beta, beta_meta, concentration_2, epsilon, prior_1, prior_2] = params
-	if alpha_2 < 0 or alpha_2 > 1 or epsilon < 0 or epsilon > 1 or prior_1 < 0 or prior_1 > 1 or prior_2 < 0 or prior_2 > 1:
+	if alpha_2 < 0 or alpha_2 > 1 or epsilon < 0 or epsilon > 1 or prior_1 < 0 or prior_1 > 1 or prior_2 < 0 or prior_2 > 1 or prior_1 + prior_2 > 1:
 		print(f'Subject: {D[0,0]}')
 		print(f"alpha_2: {alpha_2}, beta: {beta}, beta_meta: {beta_meta}, concentration_2: {concentration_2}, epsilon: {epsilon}, prior_1: {prior_1}, prior_2: {prior_2}")
 		return np.inf
