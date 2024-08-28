026a655e7b2a7b5bb5ba1f671f78b1bf4e2b7c62
Jingjing Li <jl3676@cornell.edu>
2024-08-27 22:05:58 -0500

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -583,7 +583,7 @@ def plot_validation_p_policies(data_sim, ntrials=1):
     plt.tight_layout()
     plt.legend()
 
-def plot_transfer_effect_real_vs_sim(data, sim_data_m1, sim_data_m2, cluster, start_trial=0, trials_to_probe=10, m1='Forward', m2='Backward', save_vector=False, normalize=False, first_press_accuracy=False):
+def plot_transfer_effect_real_vs_sim(data, sim_data_m1, sim_data_m2, condition, cluster, start_trial=0, trials_to_probe=10, m1='Forward', m2='Backward', save_vector=False, normalize=False, first_press_accuracy=False):
     num_subjects = data['tr'].shape[0]
     
     _, n_presses_stage_2 = helpers.calc_mean(data, start_trial=start_trial, trials_to_probe=trials_to_probe, first_press_accuracy=first_press_accuracy)
@@ -616,10 +616,10 @@ def plot_transfer_effect_real_vs_sim(data, sim_data_m1, sim_data_m2, cluster, st
     axes[1].plot(transfer_human, transfer_m2, '.', alpha=0.5, color='cornflowerblue')
     axes[0].set_title(f'{m1} model')
     axes[1].set_title(f'{m2} model')
-    plt.suptitle(f'Transfer effect, Human vs. Model, Cluster {cluster}, Trials {start_trial+1}-{start_trial+trials_to_probe}')
+    plt.suptitle(f'Transfer effect, Human vs. Model, {condition}, Cluster {cluster}, Trials {start_trial+1}-{start_trial+trials_to_probe}')
     plt.tight_layout()
 
     if save_vector:
-        plt.savefig(f'plots/transfer_effect_real_vs_sim_cluster{cluster}.svg', format='svg', dpi=1200)
+        plt.savefig(f'plots/transfer_effect_real_vs_sim_{condition}_cluster{cluster}.svg', format='svg', dpi=1200)
     else:
         plt.show()
