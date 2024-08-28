30e0b9ace30f52d1a83e33b51eb347fb39204ec1
Jingjing Li <jl3676@cornell.edu>
2024-08-27 22:16:53 -0500

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -605,18 +605,18 @@ def plot_transfer_effect_real_vs_sim(data, sim_data_m1, sim_data_m2, condition, 
     # in two subplots, plot dot plots of individual human transfer effects against model simulations, add dashed diagonal line
     fig, axes = plt.subplots(1,2,figsize=(6,3), sharex=True, sharey=True)
     for ax in axes:
-        ax.plot([-0.2,0.5],[-0.2,0.5],'--',color='gray')
+        ax.plot([-2,2],[-2,2],'--',color='gray')
         ax.set_xlabel('Human')
         ax.set_ylabel('Model')
     axes[0].plot(transfer_human, transfer_m1, '.', alpha=0.5, color='lightcoral')
     axes[1].plot(transfer_human, transfer_m2, '.', alpha=0.5, color='cornflowerblue')
+    axes[0].set_xlim([-2,2])
+    axes[0].set_ylim([-2,2])
+    axes[0].set_aspect('equal')
     axes[0].set_title(f'{m1} model')
     axes[1].set_title(f'{m2} model')
     plt.suptitle(f'Transfer effect, Human vs. Model, {condition}, Cluster {cluster}, Trials {start_trial+1}-{start_trial+trials_to_probe}')
     plt.tight_layout()
-    # make each subplot square
-    axes[0].set_aspect('equal', 'box')
-    axes[1].set_aspect('equal', 'box')
 
     # print correlation coefficients and p-values
     print(f'{m1} model:')
@@ -625,6 +625,6 @@ def plot_transfer_effect_real_vs_sim(data, sim_data_m1, sim_data_m2, condition, 
     print(stats.pearsonr(transfer_human, transfer_m2))
 
     if save_vector:
-        plt.savefig(f'plots/transfer_effect_real_vs_sim_{condition}_cluster{cluster}.svg', format='svg', dpi=1200)
+        plt.savefig(f'plots/transfer_effect_real_vs_sim_{condition}_cluster{cluster}.svg')
     else:
         plt.show()
